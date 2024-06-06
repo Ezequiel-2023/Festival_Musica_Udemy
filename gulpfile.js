4c99c3960c45dc3037c655137bde9206a900caba
Ezequiel Rivera <moiseslarios515@gmail.com>
2024-06-05 20:48:44 -0600

    se agrego sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 
-
 import { src, dest, watch } from 'gulp';
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 
 const sass = gulpSass(dartSass);
-
+// corres nuestro sitio web "npm run dev"
 export function css(done) {
     src('src/scss/app.scss')
-      .pipe(sass())
+      .pipe(sass().on('error', sass.logError))
       .pipe(dest('build/css'))
     done()
 }
-
+// escuchar cambios 
 export function dev () {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
 }
